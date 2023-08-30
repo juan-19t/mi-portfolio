b93acdacbbe6a414bec45f16a70bd78977220c35
juan-19t <juantrentino@gmail.com>
2023-08-30 16:03:59 +0200

    cambios provisorios

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let enlace1Menu = document.getElementById('enlace1');
 let seccionProyectos = document.getElementById('proyectos');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
+let indiceDiv = document.getElementById('indice');
+let contenedorProyecto = document.getElementById('contenedor-proyectos');
+let infoProyecto = document.getElementById('info-proyecto');
 let i = 0;
 
-function prueba(valor) {
-    valor.style.color = 'white';
-}
+// function prueba(valor) {
+//     valor.style.color = 'white';
+// }
 
 
 let listaProyectos = [
@@ -35,45 +36,61 @@ let listaProyectos = [
         link: "https://piedra-papel-tijera-gules.vercel.app/"
     }
 ]
-
-function creandoCarrousel(indice) {
-    return seccionProyectos.innerHTML = `<div id="info-proyecto"><h1>Mis proyectos <div id="subrayado"></div></h1>
+function  creandoCarrousel(indice) {
+    
+    infoProyecto.innerHTML = `<h1>Mis proyectos <div id="subrayado"></div></h1>
             <h2>${listaProyectos[indice].titulo}</h2>
             <p>${listaProyectos[indice].descripcion}</p>
-            <a href="${listaProyectos[indice].link}" target="_blank">Deploy</a></div>
-            </div>
-            <div id="contenedor-proyectos">
-            
+            <a href="${listaProyectos[indice].link}" target="_blank">Deploy</a></div>`;
+
+    contenedorProyecto.innerHTML = `
             <div id="detalle-proyecto">
             <img id="imagen-recorte" src=${listaProyectos[indice].imagen} alt="">
             </div>
-            </div>`;
+            `;
 }
 
+creandoCarrousel(0);
+
+let prevBtn = document.getElementById('prevBtn');
+let nextBtn = document.getElementById('nextBtn');
 
 nextBtn.addEventListener('click', () => {
-
-     if (i < listaProyectos.length - 1) {
+        
+    if (i < listaProyectos.length - 1) {
         i++;
         creandoCarrousel(i);
     } else {
         i = 0;
         creandoCarrousel(i)
-
     }
-})
+    indiceDeProyectos(i);
+}   )
 
-prevBtn.addEventListener('click',()=>{
+prevBtn.addEventListener('click', () => {
     if (i > 0) {
         i--;
-        creandoCarrousel(i);
+         creandoCarrousel(i);
     } else {
-        i = listaProyectos.length-1;
+        i = listaProyectos.length - 1;
         creandoCarrousel(i)
-
     }
+        indiceDeProyectos(i);
 })
 
-creandoCarrousel(0);
+
+
+indiceDeProyectos(i);
+function indiceDeProyectos(numero) {
+    indice = numero + 1;
+    total = listaProyectos.length;
+    indiceDiv.innerHTML = `<p id="numeros-indice">${indice}/${total}</p>`;
+}
+
+
+
+
+
+
 
 
